c6aff653284b083ac564ce85f5fb02885d77a7bc
danijelcamdzic <danijelcamdzic@gmail.com>
2023-04-29 20:05:36 +0200

    Add END_MESSAGE appending

--- app/mqtt_subscriber.cpp
+++ app/mqtt_subscriber.cpp
@@ -12,8 +12,8 @@
 
 const std::string SERVER_ADDRESS("tcp://mqtt.eclipseprojects.io:1883");
 const std::string CLIENT_ID("MQTT_CPP_Subscriber");
-const std::string SUB_TOPIC("topic/topic4");
-const std::string PUB_TOPIC("topic/topic5");
+const std::string SUB_TOPIC("/topic/topic1");
+const std::string PUB_TOPIC("/topic/topic3");
 
 class callback : public virtual mqtt::callback
 {
@@ -71,6 +71,9 @@ void publisher(mqtt::async_client& client, const std::string& public_key_filenam
             std::string encrypted_msg = encrypt_message(input, public_key_filename);
             if (!encrypted_msg.empty())
             {
+                // Append "END_MESSAGE" to the encrypted message
+                encrypted_msg += "END_MESSAGE";
+
                 auto msg = mqtt::make_message(PUB_TOPIC, encrypted_msg);
                 msg->set_qos(1);
                 client.publish(msg)->wait_for(std::chrono::seconds(10));
